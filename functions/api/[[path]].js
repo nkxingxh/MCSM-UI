781a47d2aae79e3c9290f18039b4807b3c621b29
nkxingxh <nkxingxh@nkxingxh.top>
2022-05-26 20:19:24 +0800

    Update [[path]].js

--- functions/api/[[path]].js
+++ functions/api/[[path]].js
@@ -10,7 +10,7 @@ export async function onRequest(context) {
     } = context;
 
     let url = new URL(request.url);
-    url.hostname = API_HOST;
+    url.hostname = env.API_HOST;
     let newRequest = new Request(url, request);
     return fetch(newRequest);
 }
